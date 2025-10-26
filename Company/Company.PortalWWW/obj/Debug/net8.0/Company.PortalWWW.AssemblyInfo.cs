75cc2c68d6b5922724697dbd24f67fbadc759309
Inferior Lemon <strusinskipawel@wp.pl>
2025-10-26 21:36:56 +0100

    Zmainy w INDX

--- Company/Company.PortalWWW/obj/Debug/net8.0/Company.PortalWWW.AssemblyInfo.cs
+++ Company/Company.PortalWWW/obj/Debug/net8.0/Company.PortalWWW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.PortalWWW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfeece54e0df593f909888aad52cfeb07239ff3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26240f50e3445cb023eda68e42f2475620f84ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.PortalWWW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.PortalWWW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
